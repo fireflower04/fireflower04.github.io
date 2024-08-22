80243c77ab1fbf4baf8625736cc89a18d0166f41
CanaMatsuda <kanamatsuda.16@gmail.com>
2024-08-22 17:27:40 +0900

    課題リンクを追加

---
+++ my-works/season/js/script.js
@@ -0,0 +1,8 @@
+const fullYear = new Date().getFullYear();
+console.log(fullYear);
+
+if(fullYear >= 2025){
+document.querySelector('#this-year').textContent = '2024 - '+fullYear
+}else{
+document.querySelector('#this-year').textContent = fullYear    
+}
--- season/js/script.js
+++
@@ -1,8 +0,0 @@
-const fullYear = new Date().getFullYear();
-console.log(fullYear);
-
-if(fullYear >= 2025){
-document.querySelector('#this-year').textContent = '2024 - '+fullYear
-}else{
-document.querySelector('#this-year').textContent = fullYear    
-}
